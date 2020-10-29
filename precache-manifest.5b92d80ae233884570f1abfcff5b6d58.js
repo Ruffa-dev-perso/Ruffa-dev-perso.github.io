674c019f07721d43d52ef048d828d6d28f338a44
TheRott06 <69381950+TheRott06@users.noreply.github.com>
2020-10-29 19:32:06 +0100

    init

---
+++ precache-manifest.5b92d80ae233884570f1abfcff5b6d58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee77e23bcbbe3776c7a34fa41b760045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86188f3cca88fa68e63f",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5ef257db3009a977cac9",
+    "url": "/static/css/main.a4762e15.chunk.css"
+  },
+  {
+    "revision": "86188f3cca88fa68e63f",
+    "url": "/static/js/2.23f451f0.chunk.js"
+  },
+  {
+    "revision": "98b53ee81b497d75f7c86a93c82f9a7a",
+    "url": "/static/js/2.23f451f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef257db3009a977cac9",
+    "url": "/static/js/main.cfd3c848.chunk.js"
+  },
+  {
+    "revision": "f877e1c383c0996fbee9",
+    "url": "/static/js/runtime-main.0cde328b.js"
+  }
+]);
