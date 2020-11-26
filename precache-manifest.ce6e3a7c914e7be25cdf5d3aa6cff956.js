93a21a87b68a9d80e431ba1f26538532e2c17e6d
TheRott06 <69381950+TheRott06@users.noreply.github.com>
2020-11-26 18:35:07 +0100

    try

--- precache-manifest.ce6e3a7c914e7be25cdf5d3aa6cff956.js
+++ precache-manifest.ce6e3a7c914e7be25cdf5d3aa6cff956.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "755cadfea89c7f588936bf4dd3df52ca",
+    "revision": "a2a5c8a1f3f8bc7e147ceffc99b8c013",
     "url": "/index.html"
   },
   {
-    "revision": "978650cfbbe60a572d63",
+    "revision": "86188f3cca88fa68e63f",
     "url": "/static/css/2.d9ad5f5c.chunk.css"
   },
   {
-    "revision": "8eda53242d688d0a7395",
+    "revision": "98b414f12def33d3ebf1",
     "url": "/static/css/main.a4762e15.chunk.css"
   },
   {
-    "revision": "978650cfbbe60a572d63",
-    "url": "/static/js/2.7083214f.chunk.js"
+    "revision": "86188f3cca88fa68e63f",
+    "url": "/static/js/2.23f451f0.chunk.js"
   },
   {
     "revision": "98b53ee81b497d75f7c86a93c82f9a7a",
-    "url": "/static/js/2.7083214f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.23f451f0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eda53242d688d0a7395",
-    "url": "/static/js/main.4ee23300.chunk.js"
+    "revision": "98b414f12def33d3ebf1",
+    "url": "/static/js/main.cd7b42d6.chunk.js"
   },
   {
     "revision": "f877e1c383c0996fbee9",
